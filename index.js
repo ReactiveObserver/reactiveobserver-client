a5e5dd780c0bfe0b12450db87f6b5e4c66498060
Michał Łaszczewski <michal@cloudforge.pl>
2015-10-24 13:43:58 +0200

    TimeSync removed

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 exports.reactiveApi=require('./lib/reactive-api.js')
 exports.createReactiveApi=exports.reactiveApi.createReactiveApi
 exports.ReactiveConnection = require('./lib/ReactiveConnection.js')
-exports.TimeSync = require('./../reactiveobserver-timesync/index.js')
